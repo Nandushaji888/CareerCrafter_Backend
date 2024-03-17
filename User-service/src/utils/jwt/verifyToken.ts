061153a3517c59fe42bf40a04bbcc0387fc0648d
Nandu Shaji <nanduts888@gmail.com>
2024-03-17 14:11:54 +0530

    refresh-token implemented

--- User-service/src/utils/jwt/verifyToken.ts
+++ User-service/src/utils/jwt/verifyToken.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { IUser } from '../interface/interface'
+import { createAccessToken } from "./jwt";
+import { IUser } from "../interface/interface";
 
 // Extend Request interface to include user property
 declare global {
@@ -13,27 +14,41 @@ declare global {
 
 export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
   const user_accessToken = req.cookies.user_accessToken;
-
-
-console.log('user_accessToken');
-console.log(user_accessToken);
+  const user_refreshToken = req.cookies.user_refreshToken;
 
 
   
-  if (!user_accessToken) {
-    return res.status(401).json({ status: false, message: 'Unauthorized - No token Provided"' });
-  } else {
-    jwt.verify(user_accessToken, process.env.ACCESS_SECRET_KEY || "", (err: jwt.VerifyErrors | null, decoded: any) => {
-      if (err) {
-        return res.json({ status: false, message: 'Invalid Token' });
-      } else {
-        const decodedUser = decoded.user as IUser;
-        req.user = decodedUser;
-        console.log('req.user');
-        console.log(req.user);
+
+
+  jwt.verify(user_accessToken, process.env.ACCESS_SECRET_KEY || "", (err: jwt.VerifyErrors | null, decoded: any) => {
+    if (err) {
+      console.log('err.name');
+      console.log(err.name);
+      
+      if (err.name === 'JsonWebTokenError' && user_refreshToken) {
         
-        next();
+        jwt.verify(user_refreshToken, process.env.REFRESH_SECRET_KEY || "", (errRefresh: jwt.VerifyErrors | null, decodedRefresh: any) => {
+          if (errRefresh) {
+            return res.status(401).json({ status: false, message: 'Invalid Refresh Token' });
+          }
+          const user = decodedRefresh.user;
+          const newAccessToken = createAccessToken(user, process.env.ACCESS_SECRET_KEY || "", "15m");
+          res.cookie("user_accessToken", newAccessToken, {
+            maxAge: 300000, //5 minutes in milliseconds
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict"
+          });
+          next();
+        });
+      } else {
+        return res.status(401).json({ status: false, message: 'Unauthorized - No token Provided' });
       }
-    });
-  }
+    } else {
+      const decodedUser = decoded.user as IUser;
+      req.user = decodedUser;
+      next();
+    }
+  });
 };
+
